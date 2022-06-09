8cd3027cdb6cf965ccd41738318800f7490cb212
Rizky Ananda <rizky.ananda@detik.com>
2022-06-09 11:04:15 +0700

    Add buildSrc: dependency management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,19 @@
 plugins {
-    id ("com.android.application")
-    kotlin ("android")
+    id (BuildPlugins.application)
+    kotlin (BuildPlugins.kotlin)
 }
 
 android {
-    compileSdk = 32
+    compileSdk = AndroidConfig.compileSdkVersion
 
     defaultConfig {
-        applicationId = "com.ekyrizky.gradle_kotlin_dsl"
-        minSdk = 21
-        targetSdk = 32
-        versionCode = 1
-        versionName = "1.0"
+        applicationId = AndroidConfig.applicationId
+        minSdk = AndroidConfig.minSdkVersion
+        targetSdk = AndroidConfig.targetSdkVersion
+        versionCode = AndroidConfig.versionCode
+        versionName = AndroidConfig.versionName
 
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        testInstrumentationRunner = AndroidConfig.testRunner
     }
 
     buildTypes {
@@ -36,13 +36,14 @@ android {
 
 dependencies {
 //    implementation(project(":lib"))
-    implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.appcompat:appcompat:1.4.2")
-    implementation ("com.google.android.material:material:1.6.1")
-    implementation ("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation ("androidx.navigation:navigation-fragment-ktx:2.3.5")
-    implementation ("androidx.navigation:navigation-ui-ktx:2.3.5")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.3")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.4.0")
+    implementation (Dependencies.core)
+    implementation (Dependencies.appCompat)
+    implementation (Dependencies.materialDesign)
+    implementation (Dependencies.constraintLayout)
+    implementation (Dependencies.navigationFragment)
+    implementation (Dependencies.navigationUI)
+
+    testImplementation (TestDependencies.junit)
+    androidTestImplementation (TestDependencies.junitExt)
+    androidTestImplementation (TestDependencies.espresso)
 }
